8481dcee04b27a2678239c98bd9d07d810d7f452
analaura100 <19470201@itcampeche.edu.mx>
2020-10-17 00:36:04 -0500

    Se agrego el manejo de eventos de los tres botones.

--- ImcDesktop/MainWindow.xaml.cs
+++ ImcDesktop/MainWindow.xaml.cs
@@ -23,16 +23,53 @@ namespace ImcDesktop
         public MainWindow()
         {
             InitializeComponent();
+            pesotextbox.Focus();
         }
 
         private void calcularButton_Click(object sender, RoutedEventArgs e)
         {
-
+            string s = pesotextbox.Text;
+            decimal peso = Convert.ToDecimal(s);
+            s = estaturatextbox.Text;
+            decimal estatura = Convert.ToDecimal(s);
+            decimal imc = peso / (estatura * estatura);
+            imctextblock.Text = string.Format("{0:.0000}", imc);
+            imctextblock.Foreground = SetColorEstadoNutricional(imc);
+            situaciontextblock.Text = EstadoNutricional(imc);
+            situaciontextblock.Foreground = SetColorEstadoNutricional(imc);
+            limpiarbutton.Focus();
         }
 
         private void limpiarbutton_Click(object sender, RoutedEventArgs e)
         {
-
+            pesotextbox.Text = "";
+            estaturatextbox.Text = "";
+            imctextblock.Text = "0.0";
+            imctextblock.Foreground = Brushes.Black;
+            situaciontextblock.Text = "";
+            situaciontextblock.Foreground = Brushes.Black;
+            pesotextbox.Focus();
         }
-    }
+
+        private void salirbutton_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+        private string EstadoNutricional(decimal imc)
+        {
+            if (imc < 18.5M) { return "Peso bajo"; }
+            else if (imc < 25.0M) { return "Peso normal"; }
+            else if (imc < 30.0M) { return "Sobrepeso"; }
+            else if (imc < 40.0M) { return "Obesidad"; }
+            else { return "Obesidad extrema"; }
+        }
+        private Brush SetColorEstadoNutricional(decimal imc)
+        {
+            if (imc < 18.5M) { return Brushes.Yellow; }
+            else if (imc < 25.0M) { return Brushes.Green; }
+            else if (imc < 30.0M) { return Brushes.Yellow; }
+            else if (imc < 40.0M) { return Brushes.Orange; }
+            else { return Brushes.Red; }
+        }
+    } 
 }
